90cf73e4194849fcbce1ac378094f49218932ea7
ewoudje <ewoud.dierickx@hotmail.be>
2020-11-22 12:18:45 +0100

    Part of the test game made

--- Games/TestGame/Inc/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by ewoudje on 22/11/20.
-//
-
-#ifndef ECON_DEV_MAIN_H
-#define ECON_DEV_MAIN_H
-
-#endif //ECON_DEV_MAIN_H
--- Games/TestGame/Src/game.c
+++ Games/TestGame/Src/game.c
@@ -4,21 +4,56 @@
 
 #include "EPICF/game.h"
 #include "EPICF/system.h"
+#include "platform/display.h"
 
+
+struct {
+    uint16_t posX, posY;
+    uint16_t counterX, counterY;
+}* gameState;
 
 uint16_t resume() {
     GAME_LOOP_START
+        GAME_INPUT(UP)
+        if (GAME_INPUT_PRESSED) {
+            gameState->counterY += delta;
+        }
 
+        GAME_INPUT(DOWN)
+        if (GAME_INPUT_PRESSED) {
+            gameState->counterY -= delta;
+        }
+
+        GAME_INPUT(RIGHT)
+        if (GAME_INPUT_PRESSED) {
+            gameState->counterX += delta;
+        }
+
+        if (gameState->counterX > 100) {
+            gameState->posX++;
+        }
+
+        if (gameState->counterY > 100) {
+            gameState->posY++;
+        }
     GAME_LOOP_END
 }
 
 uint16_t start() {
+    gameState = SYSTEM->currentGame;
+
+    //INIT
+    gameState->posX = DISPLAY_WIDTH / 2;
+    gameState->posY = DISPLAY_HEIGHT / 2;
+    gameState->counterX = 0;
+    gameState->counterY = 0;
+
     resume();
 }
 
 struct GameDescription TestGame = {
         .name = "Test Game",
-        .gameCSize = sizeof(void),
+        .gameCSize = sizeof(*gameState),
         .start = start,
         .resume = resume
 };
